e084734874c29b18c7c526b6026cc9fc2071bff2
Denys <denisburduzan@gmail.com>
2023-12-08 15:44:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let i = 6; 
+
+for (;i <= 1000; i++){
+    if(i % 2 === 0);
+    var string = `Число ${i} є простим числом`
+    console.log(string)
+    if(i % 2 !== 0);
+    var string = `Число ${i} не є простим числом`
+    console.log(string)
+}
